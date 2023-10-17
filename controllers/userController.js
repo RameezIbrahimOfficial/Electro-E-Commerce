8c823956169414a472a79ca19da588af8c052dca
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2023-10-17 18:29:27 +0530

    Cart Update Code Fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const customerModel = require("../Model/customer");
 const productsModel = require("../Model/product");
 const categoryModel = require("../Model/category");
-const brandsModel = require('../Model/brand')
-const cartModel = require("../Model/cart")
+const brandsModel = require("../Model/brand");
+const cartModel = require("../Model/cart");
 
 let phoneNumber;
 let isOtpVerified;
@@ -52,7 +52,7 @@ module.exports.getHome = async (req, res) => {
   try {
     const isLogin = req.cookies.isLogin;
     let products = await productsModel.find({});
-    let brands = await brandsModel.find({})
+    let brands = await brandsModel.find({});
     res.render("home-page", { products, brands, isLogin });
   } catch (err) {
     console.error(err);
@@ -76,7 +76,10 @@ module.exports.postUserRegister = async (req, res) => {
         errorMsgSignup: "Account Created Successfully",
       });
     } else {
-      res.render("page-login-register", { errorMsgOTP: "Invalid OTP", isLogin });
+      res.render("page-login-register", {
+        errorMsgOTP: "Invalid OTP",
+        isLogin,
+      });
     }
   } catch (err) {
     console.error(err);
@@ -106,7 +109,7 @@ module.exports.postUserLogin = async (req, res) => {
         if (email === user.email && password === user.password) {
           const token = jwt.sign(user.email, JWT_SECRET);
           res.cookie("userToken", token, { maxAge: 24 * 60 * 60 * 1000 });
-          res.cookie("isLogin", true, { maxAge: 24 * 60 * 60 * 1000 })
+          res.cookie("isLogin", true, { maxAge: 24 * 60 * 60 * 1000 });
           res.redirect("/products");
         } else {
           res.render("page-login", {
@@ -116,7 +119,8 @@ module.exports.postUserLogin = async (req, res) => {
       }
     } else {
       res.render("page-login", {
-        errorMsgLogin: "No User Found on this Email Please Register", isLogin
+        errorMsgLogin: "No User Found on this Email Please Register",
+        isLogin,
       });
     }
   } catch (err) {
@@ -129,7 +133,7 @@ module.exports.getProductsPage = async (req, res) => {
     const isLogin = req.cookies.isLogin;
     const products = await productsModel.find({});
     const categories = await categoryModel.find({});
-    const brands = await brandsModel.find({})
+    const brands = await brandsModel.find({});
     res.render("products-grid-view", { products, categories, brands, isLogin });
   } catch (err) {
     console.error(err);
@@ -141,37 +145,35 @@ module.exports.getProductPage = async (req, res) => {
     const isLogin = req.cookies.isLogin;
     const id = req.query.id;
     const product = await productsModel.findOne({ _id: id });
-    const brand = await brandsModel.findOne({ brandName: product.brand })
+    const brand = await brandsModel.findOne({ brandName: product.brand });
     res.render("shop-product-full", { product, brand, isLogin });
   } catch (err) {
     console.error(err);
   }
 };
 
-
-
 module.exports.getCartPage = async (req, res) => {
   try {
     const productId = req.query.productId;
     const isLogin = req.cookies.isLogin;
     const user = req.user;
-    const userId = await customerModel.findOne({email:user},{_id:1})
-    const userCart = await cartModel.findOne({userId:userId._id})
-      .populate({
-        path: 'products.productId',
-        model: 'Product'
-      });
-      let grandTotal = 0
-      for (let i = 0; i < userCart.products.length; i++) {
-      grandTotal = grandTotal + userCart.products[i].productId.salePrice * userCart.products[i].quantity
-      
+    const userId = await customerModel.findOne({ email: user }, { _id: 1 });
+    const userCart = await cartModel.findOne({ userId: userId._id }).populate({
+      path: "products.productId",
+      model: "Product",
+    });
+    let grandTotal = 0;
+    for (let i = 0; i < userCart.products.length; i++) {
+      grandTotal =
+        grandTotal +
+        userCart.products[i].productId.salePrice *
+          userCart.products[i].quantity;
     }
-    res.render('cart', { userCart, isLogin, grandTotal })
-
+    res.render("cart", { userCart, isLogin, grandTotal });
   } catch (err) {
-  console.error(err);
-  res.status(500).json({ message: 'Server error' });
-}
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports.postAddToCart = async (req, res) => {
@@ -181,7 +183,7 @@ module.exports.postAddToCart = async (req, res) => {
     if (productId) {
       const currentUser = await customerModel.findOne({ email: req.user });
       if (!currentUser) {
-        return res.redirect('/signin')
+        return res.redirect("/signin");
       }
       const userCart = await cartModel.findOne({ userId: currentUser._id });
       if (userCart) {
@@ -207,13 +209,13 @@ module.exports.postAddToCart = async (req, res) => {
         });
         await newCart.save();
       }
-      res.status(200).json({ message : "Added to Cart" })
+      res.status(200).json({ message: "Added to Cart" });
     }
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Server error' });
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
-}
+};
 
 module.exports.getDeleteCart = async (req, res) => {
   try {
@@ -224,80 +226,180 @@ module.exports.getDeleteCart = async (req, res) => {
       return res.status(404).send("User not found");
     }
     const userId = userDocument._id;
-    await cartModel.updateOne({ userId }, {
-      $pull: { products: { productId: productId } }
-    });
-    res.redirect('/cart')
+    await cartModel.updateOne(
+      { userId },
+      {
+        $pull: { products: { productId: productId } },
+      }
+    );
+    res.redirect("/cart");
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
 };
 
-module.exports.postCartUpdate = async(req, res) => {
-  try{
+// module.exports.postCartUpdate = async (req, res) => {
+//   try {
+//     const productId = req.body.productId;
+//     const quantity = Number(req.body.quantity);
+//     if (productId) {
+//       const currentUser = await customerModel.findOne({ email: req.user });
+//       if (!currentUser) {
+//         return res.redirect("/signin");
+//       }
+//       const userCart = await cartModel.findOne({ userId: currentUser._id });
+//       if (userCart) {
+//         let productIndex = -1;
+//         for (let i = 0; i < userCart.products.length; i++) {
+//           if (productId == userCart.products[i].productId) {
+//             productIndex = i;
+//             break;
+//           }
+//         }
+
+//         if (productIndex !== -1) {
+//           userCart.products[productIndex].quantity += quantity;
+//         } else {
+//           userCart.products.push({ productId, quantity: quantity });
+//         }
+
+//         await userCart.save();
+//       } else {
+//         const newCart = new cartModel({
+//           userId: currentUser._id,
+//           products: [{ productId, quantity: 1 }],
+//         });
+//         await newCart.save();
+//       }
+//       res.status(200).json({ message: "Added to Cart" });
+//     }
+//     console.log(productId, quantity);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+
+module.exports.postCartUpdate = async (req, res) => {
+  try {
     const productId = req.body.productId;
     const quantity = Number(req.body.quantity);
-    console.log(productId,quantity)
 
-  } catch ( error ){
+    if (productId && quantity) {
+      const currentUser = await customerModel.findOne({ email: req.user });
+
+      if (!currentUser) {
+        return res.redirect("/signin");
+      }
+
+      let userCart = await cartModel.findOne({ userId: currentUser._id });
+
+      if (!userCart) {
+        userCart = new cartModel({
+          userId: currentUser._id,
+          products: [{ productId, quantity }],
+        });
+      } else {
+        const productIndex = userCart.products.findIndex(product => product.productId == productId);
+
+        if (productIndex !== -1) {
+          // Update the quantity of an existing product
+          userCart.products[productIndex].quantity = quantity;
+        } else {
+          // Add a new product to the cart
+          userCart.products.push({ productId, quantity });
+        }
+      }
+
+      await userCart.save();
+      res.status(200).json({ message: "Updated Cart" });
+    } else {
+      res.status(400).json({ message: "Invalid input" });
+    }
+  } catch (error) {
     console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
 
 
 module.exports.getContactPage = (req, res) => {
   const isLogin = req.cookies.isLogin;
-  res.render('page-contact', { isLogin })
-}
+  res.render("page-contact", { isLogin });
+};
 
 module.exports.getUserLogout = async (req, res) => {
   try {
-    res.clearCookie('userToken');
-    res.clearCookie('isLogin');
-    res.redirect('/')
+    res.clearCookie("userToken");
+    res.clearCookie("isLogin");
+    res.redirect("/");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 module.exports.getSearch = async (req, res) => {
   try {
-    const product_search = req.query.product_search
+    const product_search = req.query.product_search;
     const search_category = req.query.category;
     const isLogin = req.cookies.isLogin;
     const categories = await categoryModel.find({});
-    const brands = await brandsModel.find({})
+    const brands = await brandsModel.find({});
     if (search_category) {
       const products = await productsModel.find({ category: search_category });
-      res.render("products-grid-view", { products, categories, brands, isLogin })
+      res.render("products-grid-view", {
+        products,
+        categories,
+        brands,
+        isLogin,
+      });
     }
     if (product_search) {
-      const products = await productsModel.find({ productName: { $regex: `^${product_search}`, $options: "xi" } });
-      res.render("products-grid-view", { products, categories, brands, isLogin })
+      const products = await productsModel.find({
+        productName: { $regex: `^${product_search}`, $options: "xi" },
+      });
+      res.render("products-grid-view", {
+        products,
+        categories,
+        brands,
+        isLogin,
+      });
     }
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 module.exports.postSearch = async (req, res) => {
   try {
     const categories = await categoryModel.find({});
-    const brands = await brandsModel.find({})
+    const brands = await brandsModel.find({});
     const isLogin = req.cookies.isLogin;
-    const { ram, price, brand, internal_memory, battery_capacity, operating_system } = req.body;
+    const {
+      ram,
+      price,
+      brand,
+      internal_memory,
+      battery_capacity,
+      operating_system,
+    } = req.body;
     const aggregationStages = [];
     const orConditions = [];
-    const ramArray = [], priceArray = [], internal_memoryArray = [], brandArray = [], battery_capacityArray = [], operating_systemArray = [];
+    const ramArray = [],
+      priceArray = [],
+      internal_memoryArray = [],
+      brandArray = [],
+      battery_capacityArray = [],
+      operating_systemArray = [];
 
     // Build an array of $or conditions for each field with multiple values
     if (ram && ram.length > 0) {
       if (Array.isArray(ram)) {
         orConditions.push({ ram: { $in: ram.map(Number) } });
       } else {
-        ramArray.push(ram)
-        orConditions.push({ ram: { $in: ramArray.map(Number) } })
+        ramArray.push(ram);
+        orConditions.push({ ram: { $in: ramArray.map(Number) } });
       }
     }
 
@@ -305,8 +407,8 @@ module.exports.postSearch = async (req, res) => {
       if (Array.isArray(price)) {
         orConditions.push({ salePrice: { $in: price.map(Number) } });
       } else {
-        priceArray.push(price)
-        orConditions.push({ salePrice: { $in: priceArray.map(Number) } })
+        priceArray.push(price);
+        orConditions.push({ salePrice: { $in: priceArray.map(Number) } });
       }
     }
 
@@ -314,26 +416,34 @@ module.exports.postSearch = async (req, res) => {
       if (Array.isArray(brand)) {
         orConditions.push({ brand: { $in: brand } });
       } else {
-        brandArray.push(brand)
-        orConditions.push({ brand: { $in: brandArray } })
+        brandArray.push(brand);
+        orConditions.push({ brand: { $in: brandArray } });
       }
     }
 
     if (internal_memory && internal_memory.length > 0) {
       if (Array.isArray(internal_memory)) {
-        orConditions.push({ internalMemory: { $in: internal_memory.map(Number) } });
+        orConditions.push({
+          internalMemory: { $in: internal_memory.map(Number) },
+        });
       } else {
-        internal_memoryArray.push(internal_memory)
-        orConditions.push({ internalMemory: { $in: internal_memoryArray.map(Number) } });
+        internal_memoryArray.push(internal_memory);
+        orConditions.push({
+          internalMemory: { $in: internal_memoryArray.map(Number) },
+        });
       }
     }
 
     if (battery_capacity && battery_capacity.length > 0) {
       if (Array.isArray(battery_capacity)) {
-        orConditions.push({ batteryCapacity: { $in: battery_capacity.map(Number) } });
+        orConditions.push({
+          batteryCapacity: { $in: battery_capacity.map(Number) },
+        });
       } else {
-        battery_capacityArray.push(battery_capacity)
-        orConditions.push({ batteryCapacity: { $in: battery_capacityArray.map(Number) } });
+        battery_capacityArray.push(battery_capacity);
+        orConditions.push({
+          batteryCapacity: { $in: battery_capacityArray.map(Number) },
+        });
       }
     }
 
@@ -341,7 +451,7 @@ module.exports.postSearch = async (req, res) => {
       if (Array.isArray(operating_system)) {
         orConditions.push({ category: { $in: operating_system } });
       } else {
-        operating_systemArray.push(operating_system)
+        operating_systemArray.push(operating_system);
         orConditions.push({ category: { $in: operating_systemArray } });
       }
     }
@@ -358,23 +468,31 @@ module.exports.postSearch = async (req, res) => {
     // Check if there are any aggregation stages before attempting the aggregation
     if (aggregationStages.length > 0) {
       const products = await productsModel.aggregate(aggregationStages);
-      res.render("products-grid-view", { products, categories, brands, isLogin })
+      res.render("products-grid-view", {
+        products,
+        categories,
+        brands,
+        isLogin,
+      });
     } else {
       const products = await productsModel.find({});
-      res.render("products-grid-view", { products, categories, brands, isLogin })
+      res.render("products-grid-view", {
+        products,
+        categories,
+        brands,
+        isLogin,
+      });
     }
-
-
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 module.exports.getPorfile = async (req, res) => {
   try {
-    console.log(req.user)
-    res.send('Welcome to Profile user :-  ' + req.user)
+    console.log(req.user);
+    res.send("Welcome to Profile user :-  " + req.user);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
