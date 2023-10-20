55729fc4d40e87adc03f1a22983a8b3e68d45e20
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2023-10-20 11:51:51 +0530

    Space removed

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -41,7 +41,6 @@ userRouter.get('/wishlist/delete',userAuth.isUserLogin, userController.getDelete
 userRouter.get('/checkout', userAuth.isUserLogin, userController.getCheckoutPage)
 userRouter.get('/placeorder', userAuth.isUserLogin, userController.getPlaceOrder)
 userRouter.get('/invoice',userAuth.isUserLogin, userController.getInvoice)
-
 userRouter.get('/order/cancel', userAuth.isUserLogin, userController.getOrderCancel)
 userRouter.get('/order/return', userAuth.isUserLogin, userController.getOrderReturn)
 
