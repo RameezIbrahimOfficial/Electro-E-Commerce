db63c4c48ae6798b441de2dd400ad51f73aa17c4
RameezIbrahimOfficial <rameezibrahimofficial@gmail.com>
2023-11-06 18:44:03 +0530

    Splitted User Controller

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,11 +1,8 @@
 const express = require("express");
-
-const userRouter = express.Router();
-
-const userController = require('../controllers/userController')
 const cookieParser = require("cookie-parser");
-
+const userRouter = express.Router();
 const {userMiddlewares, userAuth} = require('../middlewares');
+const {getPlaceOrderWallet, getWalletPage, postRedeemCoupon, getPlaceOrderCOD, getPlaceOrderOnline, postUpdatePaymentStatus, getInvoice, getOrderCancel, getOrderReturn, getCheckoutPage, getWishlistPage, postAddToWishlist, getDeleteWishlist,getProductPage,getProductsPage,getContactPage, getSearch, postSearch,getCartPage,postAddToCart,getDeleteCart,postCartUpdate,getUserLogin,postUserLogin,getSendOtp,getVerifyOtp,postUserRegister,getUserRegister,getProfile,getAddAddressPage,postAddAddress,getAddressDelete,postAddressEdit,getAddressEdit,postUpdateUserDetails,getchangePasswordPage,postNewPassword,getPasswordResetPage,getSendOtpPasswordReset,getVerifyOtpPasswordReset, getHome, getUserLogout} = require('../controllers/user')
 
 userRouter.use(express.static("Public"));
 userRouter.use('/address', express.static('Public'));
@@ -13,50 +10,67 @@ userRouter.use(express.json());
 userRouter.use(express.urlencoded({ extended: true }));
 userRouter.use(cookieParser())
 
-userRouter.get("/send_otp", userController.getSendOtp);
-userRouter.get("/verify_otp", userController.getVerifyOtp);
-userRouter.get("/", userController.getHome);
-userRouter.get("/register", userController.getUserRegister);
-userRouter.post("/register", userController.postUserRegister);
-userRouter.get('/signin', userController.getUserLogin)
-userRouter.post("/signin", userController.postUserLogin);
-userRouter.get("/products", userController.getProductsPage);
-userRouter.get('/product', userController.getProductPage)
-userRouter.get('/contact', userController.getContactPage)
-userRouter.get('/search', userController.getSearch)
-userRouter.post('/search', userController.postSearch)
-userRouter.get('/cart', userAuth.isUserLogin,userAuth.isUserBloked, userController.getCartPage)
-userRouter.post('/cart', userAuth.isUserLogin,userAuth.isUserBloked, userController.postAddToCart)
-userRouter.get('/cart/delete', userAuth.isUserLogin,userAuth.isUserBloked, userController.getDeleteCart)
-userRouter.post('/cart/update', userAuth.isUserLogin,userAuth.isUserBloked, userController.postCartUpdate)
-userRouter.get('/profile',userAuth.isUserLogin,userAuth.isUserBloked, userController.getProfile)
-userRouter.get('/logout', userController.getUserLogout)
-userRouter.get('/address/add', userAuth.isUserLogin,userAuth.isUserBloked, userController.getAddAddressPage)
-userRouter.post('/address/add', userAuth.isUserLogin,userAuth.isUserBloked, userController.postAddAddress)
-userRouter.get('/address/delete',userAuth.isUserLogin,userAuth.isUserBloked, userController.getAddressDelete)
-userRouter.get('/address/edit',userAuth.isUserLogin,userAuth.isUserBloked, userController.getAddressEdit)
-userRouter.post('/address/edit',userAuth.isUserLogin,userAuth.isUserBloked, userController.postAddressEdit)
-userRouter.get('/wishlist',userAuth.isUserLogin,userAuth.isUserBloked, userController.getWishlistPage)
-userRouter.post('/wishlist',userAuth.isUserLogin,userAuth.isUserBloked, userController.postAddToWishlist)
-userRouter.get('/wishlist/delete',userAuth.isUserLogin,userAuth.isUserBloked, userController.getDeleteWishlist)
-userRouter.get('/checkout', userAuth.isUserLogin,userAuth.isUserBloked, userController.getCheckoutPage)
-userRouter.get('/placeorder/cod', userAuth.isUserLogin,userAuth.isUserBloked, userController.getPlaceOrderCOD)
-userRouter.get('/invoice',userAuth.isUserLogin,userAuth.isUserBloked, userController.getInvoice)
-userRouter.get('/order/cancel', userAuth.isUserLogin,userAuth.isUserBloked, userController.getOrderCancel)
-userRouter.get('/order/return', userAuth.isUserLogin,userAuth.isUserBloked, userController.getOrderReturn)
-userRouter.get('/placeorder/online', userAuth.isUserLogin,userAuth.isUserBloked, userController.getPlaceOrderOnline)
-userRouter.post('/updatePaymentStatus', userAuth.isUserLogin,userAuth.isUserBloked, userController.postUpdatePaymentStatus)
-userRouter.get('/forgetPassword', userController.getPasswordResetPage)
-userRouter.get('/forgetPassword/sendOtp', userController.getSendOtpPasswordReset)
-userRouter.get('/forgetPassword/verifyOtp', userController.getVerifyOtpPasswordReset)
-userRouter.get('/changePassword', userController.getchangePasswordPage);
-userRouter.post('/changePassword', userController.postNewPassword)
-userRouter.post('/profile/update', userAuth.isUserLogin, userAuth.isUserBloked, userController.postUpdateUserDetails)
-userRouter.post('/coupon/redeem', userAuth.isUserLogin, userAuth.isUserBloked, userController.postRedeemCoupon)
-userRouter.get('/wallet', userAuth.isUserLogin, userAuth.isUserBloked, userController.getWalletPage)
+// User 
+userRouter.get("/send_otp", getSendOtp);
+userRouter.get("/verify_otp", getVerifyOtp);
+userRouter.get("/", getHome);
+userRouter.get("/register", getUserRegister);
+userRouter.post("/register", postUserRegister);
+userRouter.get('/signin', getUserLogin)
+userRouter.post("/signin", postUserLogin);
+userRouter.get('/forgetPassword', getPasswordResetPage)
+userRouter.get('/forgetPassword/sendOtp', getSendOtpPasswordReset)
+userRouter.get('/forgetPassword/verifyOtp', getVerifyOtpPasswordReset)
+userRouter.get('/changePassword', getchangePasswordPage);
+userRouter.get('/logout', getUserLogout)
+userRouter.get('/profile',userAuth.isUserLogin,userAuth.isUserBloked, getProfile)
+userRouter.get('/address/add', userAuth.isUserLogin,userAuth.isUserBloked, getAddAddressPage)
+userRouter.post('/address/add', userAuth.isUserLogin,userAuth.isUserBloked, postAddAddress)
+userRouter.get('/address/delete',userAuth.isUserLogin,userAuth.isUserBloked, getAddressDelete)
+userRouter.get('/address/edit',userAuth.isUserLogin,userAuth.isUserBloked, getAddressEdit)
+userRouter.post('/address/edit',userAuth.isUserLogin,userAuth.isUserBloked, postAddressEdit)
+userRouter.post('/changePassword', postNewPassword)
+userRouter.post('/profile/update', userAuth.isUserLogin, userAuth.isUserBloked, postUpdateUserDetails)
 
+// Products
+userRouter.get("/products", getProductsPage);
+userRouter.get('/product', getProductPage)
 
-userRouter.get('/placeorder/wallet', userAuth.isUserLogin,userAuth.isUserBloked, userController.getPlaceOrderWallet)
+// Contact
+userRouter.get('/contact', getContactPage)
+
+// Search
+userRouter.get('/search', getSearch)
+userRouter.post('/search', postSearch)
+
+// Cart
+userRouter.get('/cart', userAuth.isUserLogin,userAuth.isUserBloked, getCartPage)
+userRouter.post('/cart', userAuth.isUserLogin,userAuth.isUserBloked, postAddToCart)
+userRouter.get('/cart/delete', userAuth.isUserLogin,userAuth.isUserBloked, getDeleteCart)
+userRouter.post('/cart/update', userAuth.isUserLogin,userAuth.isUserBloked, postCartUpdate)
+
+// Wishlist
+userRouter.get('/wishlist',userAuth.isUserLogin,userAuth.isUserBloked, getWishlistPage)
+userRouter.post('/wishlist',userAuth.isUserLogin,userAuth.isUserBloked, postAddToWishlist)
+userRouter.get('/wishlist/delete',userAuth.isUserLogin,userAuth.isUserBloked, getDeleteWishlist)
+
+// Checkout
+userRouter.get('/checkout', userAuth.isUserLogin,userAuth.isUserBloked, getCheckoutPage)
+
+// Order
+userRouter.get('/placeorder/cod', userAuth.isUserLogin,userAuth.isUserBloked, getPlaceOrderCOD)
+userRouter.get('/invoice',userAuth.isUserLogin,userAuth.isUserBloked, getInvoice)
+userRouter.get('/order/cancel', userAuth.isUserLogin,userAuth.isUserBloked, getOrderCancel)
+userRouter.get('/order/return', userAuth.isUserLogin,userAuth.isUserBloked, getOrderReturn)
+userRouter.get('/placeorder/online', userAuth.isUserLogin,userAuth.isUserBloked, getPlaceOrderOnline)
+userRouter.post('/updatePaymentStatus', userAuth.isUserLogin,userAuth.isUserBloked, postUpdatePaymentStatus)
+
+// Coupon
+userRouter.post('/coupon/redeem', userAuth.isUserLogin, userAuth.isUserBloked, postRedeemCoupon)
+
+// Wallet
+userRouter.get('/wallet', userAuth.isUserLogin, userAuth.isUserBloked, getWalletPage)
+userRouter.get('/placeorder/wallet', userAuth.isUserLogin,userAuth.isUserBloked, getPlaceOrderWallet)
 
 // userRouter.use(userMiddlewares.errorHandlingMiddleware)
 
